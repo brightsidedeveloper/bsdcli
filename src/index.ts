4e66ff520bb0511b7c643bf4f0cbb5bb5991100b
tim <tim@brightsidedeveloper.com>
2024-08-19 21:18:02 -0500

    fire syntax

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { program } from 'commander'
 import genBrightBase from './commands/genBrightBase'
 import createBrightSideApp from './commands/createBrightSideApp'
 
-program.command('gen-brightbase').description('Generate types and instantiate tables for BrightBase').action(genBrightBase)
+program.command('genbb').description('Generate types and instantiate tables for BrightBase').action(genBrightBase)
 
-program.command('create-brightside-app <name>').description('Create a new Brightside app').action(createBrightSideApp)
+program.command('dsb <name>').description('Create a new Brightside app').action(createBrightSideApp)
 
 program.parse(process.argv)
